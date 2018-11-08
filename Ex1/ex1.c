c4aac15f1fba77daa668bbf6305690b19bb49f9a
Bogdan <bogdan.v.balan@gmail.com>
2018-11-08 06:54:54 -0800

    Add test vector

--- Ex1/ex1.c
+++ Ex1/ex1.c
@@ -4,12 +4,19 @@
 #include <stdio.h>
 #include <time.h>
 
-#define NUM_THREADS 5   // The number of threads that are created
+#define NUM_THREADS 10   // The number of threads that are created
 #define MAX_CALLS 10 	   // The maximum number of calls to a function of linked_list performed by each thread
 
 pthread_barrier_t barrier; // The barrier used to sync all threads
 pthread_mutex_t mutex;     // Mutex used to protect the head of the list
 
+int result_check[2000]; //A vector used to check the results
+int result_index = 0;   //The index of the result vector
+int result_length = 0;
+void add_to_result(int val);
+void remove_from_result(int val);
+void sort_vector();
+	
 void* thread_routine(void* data){
 	int thd_id = (int) data;
 	int rc;
@@ -31,29 +38,64 @@ void* thread_routine(void* data){
 			pthread_mutex_lock(&mutex);
 			printf("Thread %d executes sort()\n", thd_id);
 			sort();
+			sort_vector();                      // test function
 			pthread_mutex_unlock(&mutex);
 		}
 		else if(type_of_call == 2){
 			pthread_mutex_lock(&mutex);
 			printf("Thread %d executes delete(%d)\n", thd_id,parameter);
 			delete(parameter);
+			remove_from_result(parameter);   // test function
 			pthread_mutex_unlock(&mutex);
 		}
 		else if(type_of_call == 3){
 			printf("Thread %d executes print()\n", thd_id);
-			//print();
+			print();
 		}
 		else{
 			printf("Thread %d executes add(%d)\n", thd_id,parameter);
 			add(parameter);
+			add_to_result(parameter);  //test function
 		}
 
 		no_of_operations--;
 	}
 }
 
+/*Functions used to manage the result vector*/
+void add_to_result(int val){
+	result_check[result_index] = val;
+	result_index++;
+	result_length++;
+}
+void remove_from_result(int val){
+   int i,j; 
+   for (i=0; i<result_length; i++) 
+      if (result_check[i] == val) 
+         break; 
+   if (i < result_length) 
+   { 
+     result_length--;
+     for (j=i; j<result_length; j++) 
+        result_check[j] = result_check[j+1]; 
+   } 
+}
+
+void sort_vector(){
+	int j,k,temp;
+	for(j=0;j<result_index-1;j++){
+		for(k=j;k<result_index;k++){
+			if(result_check[j] > result_check[k]){
+				temp = result_check[j];
+				result_check[j] = result_check[k];
+				result_check[k] = temp;
+			}
+		}
+	}
+}
+
 int main(){
-	int i;
+	int i,j;
 	pthread_t tids[NUM_THREADS];
 
 	pthread_barrier_init(&barrier, NULL, NUM_THREADS);
@@ -73,6 +115,10 @@ int main(){
 	printf("The list before flushing.\n");
 	print();
 	flush();
-
+	
+	printf("The result vector.\n");
+	for(j=0;j<result_length;j++){
+		printf("%d\n", result_check[j]);
+	}
 	return 0;
 }
