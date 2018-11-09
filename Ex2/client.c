d07183274b9932941232e46ccc7bd3991b6532b9
Bogdan <bogdan.v.balan@gmail.com>
2018-11-09 05:53:52 -0800

    Add request from client to reserve seats.

--- Ex2/client.c
+++ Ex2/client.c
@@ -7,13 +7,20 @@
 #include <sys/stat.h>
 #include <mqueue.h>
 
+#define SERVER_NAME   "/server"
 #define QUEUE_PERMISSIONS 0660
 #define MAX_MESSAGES 10
-#define MAX_MSG_SIZE 256
+
+typedef struct msg{
+    int name;
+    int no_of_seats;
+}message;
 
 int main (){
-    char client_name [64];
-    mqd_t mqclient;   // queue descriptor
+    char client_name[64];
+    mqd_t mqclient, mqserver;   // queue descriptor
+    message msgsnt; // the message that will be sent
+    msgsnt.name = getpid();
 
     sprintf (client_name, "/%d ", getpid());
 
@@ -21,14 +28,23 @@ int main (){
 
     attr.mq_flags = 0;
     attr.mq_maxmsg = MAX_MESSAGES;
-    attr.mq_msgsize = MAX_MSG_SIZE;
+    attr.mq_msgsize = sizeof(msgsnt);
     attr.mq_curmsgs = 0;
 
-    if ((mqclient = mq_open (client_name, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1) {
+    if ((mqclient = mq_open (client_name, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1) {  // Create a queue on which the server can send the response
         perror ("Client mq_open");
         exit (1);
     }
-    else{
-        printf("Client has created the queue.\n ");
+
+    if((mqserver = mq_open (SERVER_NAME, O_WRONLY)) == -1){  // Open the server queue to send the request
+        perror("Client mq_open on server");
     }
-}
+
+    printf("How many seats you want to reserve ? \n");    // Get the number of seats that will be reserved
+    scanf("%d",&msgsnt.no_of_seats);
+
+    if(mq_send(mqserver, (char*) &msgsnt, sizeof(msgsnt), 0) == -1){
+        perror("Client not able to send message to server.\n");
+        exit(1);
+    }
+  }
--- Ex2/server.c
+++ Ex2/server.c
@@ -9,25 +9,46 @@
 #define SERVER_NAME   "/server"
 #define QUEUE_PERMISSIONS 0660
 #define MAX_MESSAGES 10
-#define MAX_MSG_SIZE 256
 #define MAX_NO_OF_SEATS 200
+
+typedef struct msg{
+    int name;
+    int no_of_seats;
+}message;
 
 int main (){
     mqd_t mqserver;   // Queue descriptor
-    int next_seat = 1; // The number of the next seat that will be given to the client
+    int seats_taken = 0; // The number of the next seat that will be given to the client
+    message msgrcv; //Used to store the message received from client
+    char* msg_in[sizeof(msgrcv)];  // Size of message to be received
 
     struct mq_attr attr;
 
     attr.mq_flags = 0;
     attr.mq_maxmsg = MAX_MESSAGES;
-    attr.mq_msgsize = MAX_MSG_SIZE;
+    attr.mq_msgsize = sizeof(msgrcv);
     attr.mq_curmsgs = 0;
 
-    if ((mqserver = mq_open (SERVER_NAME, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1) {
+    if ((mqserver = mq_open (SERVER_NAME, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1) { // Create the message queue on which the clients send the request
         perror ("Server mq_open");
         exit (1);
     }
-    else{
-        printf("Server has created the queue.\n");
+
+    while(1){
+        if (mq_receive (mqserver,(char *) &msgrcv,(int) msg_in, NULL) == -1) {  // Get the seat reservation from the client
+            perror ("Server: mq_receive");
+            exit (1);
+        }
+
+        printf("Received a request for %d seats from %d .\n",msgrcv.no_of_seats,msgrcv.name);
+        printf("Currently there are %d seats available.\n",MAX_NO_OF_SEATS - seats_taken);
+
+        if((MAX_NO_OF_SEATS - seats_taken) >= msgrcv.no_of_seats){                        // Check if there are enough seats available
+            printf("Reserving %d seats for %d \n",msgrcv.no_of_seats,msgrcv.name);
+            seats_taken += msgrcv.no_of_seats;
+        }
+        else{
+            printf("Not enough seats avilable for the request from %d\n", msgrcv.name);
+        }
     }
 }
