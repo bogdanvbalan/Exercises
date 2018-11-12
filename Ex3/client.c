4260ed60c683dd39423ccfcc8849761e51ed44df
Bogdan Balan <bogdan.v.balan@gmail.com>
2018-11-12 06:50:11 -0800

    Add client part

--- Ex3/client.c
+++ Ex3/client.c
@@ -1,1 +1,38 @@
-//Initial version of client file
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define PORT 20000
+
+int main() {
+	int client_sock;
+	struct sockaddr_in addr; // address for client socket
+	struct sockaddr_in serv_addr; //address for server socket
+
+	char* msg = "Hello";
+
+	if ((client_sock = socket(AF_INET, SOCK_STREAM,0)) < 0) {
+		perror("Client socket create");
+		exit(EXIT_FAILURE);
+	}
+
+	memset(&serv_addr, '0', sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	if (inet_pton (AF_INET, "127.0.0.1", &serv_addr.sin_addr) <=0){
+		perror("Client add convert");
+		exit(EXIT_FAILURE);
+	}
+
+	if (connect(client_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		perror("Client connection");
+		exit(EXIT_FAILURE);
+	}
+
+	send( client_sock, msg, strlen(msg), 0);
+
+}
--- Ex3/server.c
+++ Ex3/server.c
@@ -10,30 +10,35 @@ int main() {
 	int server_fd, new_socket; // socket descriptors for server
 	struct sockaddr_in address; // address used on the socket
 	size_t address_len = sizeof(address);
+	int valread;
+	char buffer[1024] = {0};
 
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT);
 
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("Server: socket descriptor failed.");
+		perror("Server socket create.");
 		exit(EXIT_FAILURE);
 	}
 
 	if (bind(server_fd, (struct sockaddr *) &address, address_len) < 0) {
-		perror("Server: bind socket to add");
+		perror("Server bind socket to add");
 		exit(EXIT_FAILURE);
 
 	}
 
 	if (listen (server_fd, 5) < 0) {
-		perror("Server: set the socket to listen mode.");
+		perror("Server set the socket to listen mode.");
 		exit(EXIT_FAILURE);
 	}
 
 	if ((new_socket = accept(server_fd, (struct  sockaddr *) &address, (socklen_t *) &address_len)) < 0) {
-		perror("Server: accept");
+		perror("Server accept");
 		exit(EXIT_FAILURE);
 	}
+
+	valread = read(new_socket, buffer, 1024);
+	printf("Message received: %s", buffer);
 	return 0;
 }
