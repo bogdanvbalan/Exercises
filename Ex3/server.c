a176d4ecc8149760f5f23bc4a13ea99e0467dfd7
Bogdan Balan <bogdan.v.balan@gmail.com>
2018-11-12 04:08:47 -0800

    Add socket file descriptor on server

--- Ex3/server.c
+++ Ex3/server.c
@@ -1,1 +1,16 @@
-//Initial version of server file
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+
+
+int main() {
+
+	int server_fd; // socket file descriptor for server
+
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+		perror("Server: socket failed.");
+		exit(EXIT_FAILURE);
+	}
+
+	return 0;
+}
